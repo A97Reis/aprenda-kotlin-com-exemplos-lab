3cd06edd6858209bc1336a907a0baba53d56639b
A97Reis <44835214+A97Reis@users.noreply.github.com>
2022-12-15 21:49:24 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -19,3 +19,36 @@ fun main() {
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
+//--------------------------------------------------------------------------------------------------------
+
+// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+
+enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+
+class Usuario
+
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60) {
+    
+    fun cadastraConteudoEducacional(nome: String, duracao: Int) {
+        println("Digite o nome do conteúdo educacional")
+        val 
+    } 
+}
+
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+
+    val inscritos = mutableListOf<Usuario>()
+    val nivel: String
+    
+    
+    fun matricular(usuario: Usuario) {
+        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        
+        
+    }
+}
+
+fun main() {
+    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+}
